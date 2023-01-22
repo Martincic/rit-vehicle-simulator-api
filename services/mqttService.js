8570f0231a038ddddc81869e74aea8c44d4d40fd
Martincic <tomas.martincic@q.agency>
2023-01-22 18:20:25 +0100

    bugfix

--- services/mqttService.js
+++ services/mqttService.js
@@ -7,7 +7,7 @@ const options = {
     host: process.env.HOST,
     port: process.env.PORT,
     protocol: 'mqtts',
-    username: process.env.USER,
+    username: process.env.RIMACUSER,
     password: process.env.PASSWORD,
 }
 const client = mqtt.connect(options); 
@@ -19,7 +19,6 @@ export default class
     {
         let message = `{"${field}":"${value}"}`;
         let carID = `rimacWebTeam/${ID}`;
-
 
         client.publish(carID, message, [{ retain: true}, { qos: 2}]);
         client.end()
