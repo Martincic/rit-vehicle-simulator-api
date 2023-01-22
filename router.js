07612a0858734f8c3ce4ee84e0dd42f443fc0ff0
Martincic <tomas.martincic@q.agency>
2023-01-22 22:30:47 +0100

    remove example, deploy

--- router.js
+++ router.js
@@ -8,6 +8,5 @@ export var router = {
     speed: (args) => controller.speed(args),
     HVAC: (args) => controller.HVAC(args),
     latitude: (args) => controller.latitude(args),
-    longitude: (args) => controller.longitude(args),
-    test: () => { return "Hello World!" }
+    longitude: (args) => controller.longitude(args)
 };
--- schemas/master.js
+++ schemas/master.js
@@ -3,11 +3,10 @@ import {buildSchema} from 'graphql';
 // Construct a schema, using GraphQL schema language
 export var schema = buildSchema(`
   type Query {
-    speed(speed: Int): Int
+    speed(speed: Int): Int,
     HVAC(status: Boolean): Boolean,
     stateOfCharge(state: Int): Int,
     latitude(lat: Float): Float,
-    longitude(lon: Float): Float,
-    test: String
+    longitude(lon: Float): Float
   }
 `);
