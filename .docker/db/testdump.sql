c39f1b5f2b3f40fbd2485624ff7f014dfb7f1bcc
Martincic <tomas.martincic@q.agency>
2023-02-01 14:37:39 +0100

    database v1

--- .docker/db/testdump.sql
+++ .docker/db/testdump.sql
@@ -1,11 +1,24 @@
-CREATE TABLE Student(
-	student_id INT PRIMARY KEY AUTO_INCREMENT,
-	student_name VARCHAR(60),
-	student_age INT
+CREATE TABLE users(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	full_name VARCHAR(60) NOT NULL,
+	email VARCHAR(100) NOT NULL,
+	password VARCHAR(200) NOT NULL,
+	login_token VARCHAR(300) DEFAULT NULL
 );
 
-INSERT INTO Student(student_name, student_age) VALUES("Shubham verma", 21);
-INSERT INTO Student(student_name, student_age) VALUES("Utkarsh verma", 23);
+CREATE TABLE cars(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	user_id INT NOT NULL,
+	nickname VARCHAR(50) NOT NULL,
+	description VARCHAR(300) NOT NULL
+);
+
+INSERT INTO users(id, full_name, email, password) VALUES(1, "John Doe", "john@doe.com", "test123");
+INSERT INTO users(id, full_name, email, password) VALUES(2, "Ash Ketchup", "ash@mail.com", "p455word");
+
+INSERT INTO cars(id, user_id, nickname, description) VALUES(1, 1, "Nevera #1", "My trusty old Nevera");
+INSERT INTO cars(id, user_id, nickname, description) VALUES(2, 1, "Traktor", "My trusty old Traktor");
+INSERT INTO cars(id, user_id, nickname, description) VALUES(3, 1, "Tomos APN", "My trusty old Tomos");
 
 ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'password';
 
