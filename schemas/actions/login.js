c3b1e280dd369f55ca3710c1c3bb1fa7ebcfbc76
Martincic <tomas.martincic@q.agency>
2023-02-01 21:00:16 +0100

    FIXED FINALLY

--- schemas/actions/login.js
+++ schemas/actions/login.js
@@ -2,6 +2,8 @@ import { GraphQLString } from "graphql"
 import { UserType } from "../../schemas/objects/user.js"
 import { connection } from "../../services/database.js"
 
+let stuff_i_want = null;
+
 export const loginField = {
     type: UserType,
     args: {
@@ -9,38 +11,69 @@ export const loginField = {
         email: { type: GraphQLString },
         password: { type: GraphQLString },
     },
-    resolve(parent, args) {
-        // Graph to trigger this function: 
-        // query{
-        //  loginDrek(email:"bla@bla.com", name: "teehe", password: "test"){
-        //     email, name
-        //  }
-        // }
-    
-        // This return works!
-        // return {name: "test", email: args.email, password: "dada"};
-
-        // This connection.connect() function is asynchronus and wont give me results back
-        // How the hell do we get results to this 'i_want_results' variable???
-        let i_want_results;
-
-        i_want_results = connection.connect(async function (error) {
-            if (!error) {
-                console.log('Connected!');
-                return connection.query('SELECT * FROM `users` WHERE `id` = "1"', function (_error, results) {
-                    
-                    // The results do land here
-                    console.log(results[0]);
-
-                    // This return does not work
-                    return { name: "test", email: "teehe", password: "dada" };
-
-                    // I WANT TO RETURN THESE RESULTS BACK
-                    // return results[0];
-                });
+    async resolve(parent, args) {
+        return new Promise((resolve, reject) => {
+          connection.query('SELECT * FROM `users` WHERE `id` = "1"', function (_error, results) {
+            if (_error) {
+              reject(_error);
             }
+            resolve(results[0]);
+          });
         });
-      
-        return i_want_results
+      }
     }
-};
+    // resolve(parent, args) {
+    //     // Graph to trigger this function: 
+    //     // query{
+    //     //  loginDrek(email:"bla@bla.com", name: "teehe", password: "test"){
+    //     //     email, name
+    //     //  }
+    //     // }
+    
+    //     // This connection.query() function is asynchronus and wont give me results back
+    //     // How the hell do we get results to this 'i_want_results' variable???
+    //     // let i_want_results;
+
+    //     // This returns undefined
+    //     return connection.query('SELECT * FROM `users` WHERE `id` = "1"', function (_error, results) {
+                    
+    //         // The results do land here
+    //         console.log("Results:", results);
+
+    //         // This return does not work
+    //         return results;
+    //     });
+    // }
+      
+        // return i_want_results
+
+
+        // let user = get_info(null, resolve(parent, args) {
+        //     return result
+        // });
+
+        // console.log("FINAL:", stuff_i_want)
+//         let users = getUsers().then(() => {
+//             return users;
+//         });
+
+//         console.log("users: ",users);
+//         return users;
+//     }
+// };
+
+let getUsers = () => {
+    return new Promise(function(resolve, reject){
+      connection.query(
+          "SELECT * FROM users", 
+          function(err, rows){                                                
+              if(rows === undefined){
+                  reject(new Error("Error rows is undefined"));
+              }else{
+                  resolve(rows);
+              }
+          }
+      )}
+  )}
+
+
